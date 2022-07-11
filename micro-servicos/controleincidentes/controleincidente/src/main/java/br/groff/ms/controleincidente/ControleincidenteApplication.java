ce18b878f0f49a7cacaad920a31e12ea4ddd3b16
Guilherme Groff <guilherme.pgb@hotmail.com>
2022-07-10 21:39:56 -0300

    Commit Inicial
    Signed-off-by: Guilherme Groff <guilherme.pgb@hotmail.com>

---
+++ micro-servicos/controleincidentes/controleincidente/src/main/java/br/groff/ms/controleincidente/ControleincidenteApplication.java
@@ -0,0 +1,13 @@
+package br.groff.ms.controleincidente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleincidenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleincidenteApplication.class, args);
+	}
+
+}
---
+++ micro-servicos/controleincidentes/controleincidente/src/test/java/br/groff/ms/controleincidente/ControleincidenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.groff.ms.controleincidente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleincidenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
